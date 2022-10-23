3b703d259893c09163c932f6a594bacf7bc5bdcb
ヒューマンワーキング <coolxiaom95@gmail.com>
2022-10-23 17:29:08 +0800

    function

---
+++ src/main/kotlin/function/FunctionManager.kt
@@ -0,0 +1,87 @@
+package com.sakurawald.plum.reloaded.function
+
+import com.sakurawald.plum.reloaded.Plum
+import com.sakurawald.utils.DateUtil
+import com.sakurawald.utils.NumberUtil
+import java.util.*
+
+abstract class FunctionManager {
+    /** 功能使用的历史记录者  */
+    val functionUseHistoryManager = HistoryManager<Long>(
+        "功能使用历史记录者", 0 - NumberUtil.getBigEnoughNumber()
+    )
+
+    /** 判断发送间隔是否合法  */
+    abstract fun canUse(QQGroup: Long): Boolean
+    fun updateUseTime(QQGroup: Long) {
+        functionUseHistoryManager.updateCall_History(QQGroup)
+    }
+}
+
+
+/**
+ * 用于记录调用历史的对象
+ */
+class HistoryManager<E>(
+    /**
+     * 该调用历史管理者的名称
+     */
+    val history_manager_name: String?,
+    /**
+     * 表示当第一次调用时, 默认设置的last_call_date与当前时间的时间偏移(sec)
+     */
+    val firstCall_sec_increment: Int
+) {
+    /**
+     * 要求每次调用至少需要间隔多少时间, 防止恶意调用
+     */
+    private val call_history = HashMap<E, Calendar?>()
+
+    private fun distance_last_call_seconds(user: E): Long {
+        return DateUtil.diffSeconds(
+            Calendar.getInstance(),
+            getCallHistory(user)
+        )
+    }
+
+    private fun getCallHistory(user: E): Calendar? {
+        Plum.logger.debug("Get Call History: user = $user")
+
+        // 获取某个user号的调用历史，若获取的是Null，则说明这是该用户第一次调用，应返回一个很老的日期，让该用户可以正常发车。
+        var result = call_history[user]
+        if (result == null) {
+            val default_calendar = Calendar.getInstance()
+            default_calendar[Calendar.SECOND] += firstCall_sec_increment
+            result = default_calendar
+
+            // [!] 若不存在, 则手动设置一个
+            call_history[user] = result
+        }
+        return result
+    }
+
+    private val logDebugType: String
+        get() = "调用历史管理者 - $history_manager_name"
+
+    /**
+     * 输入时间间隔, 判断某个用户的两次调用间隔是否合法
+     */
+    fun isCallSuccessIntervalLegal(user: E, limit_sec: Int): Boolean {
+        val diff = distance_last_call_seconds(user)
+        val limit = limit_sec.toLong()
+        Plum.logger.debug("判断当前时间的调用是否时间间隔合法: diff = $diff, limit = $limit")
+        return diff >= limit
+    }
+
+    fun setCall_History(user: E, calendar: Calendar) {
+        Plum.logger.debug("设置调用历史: user = $user, calendar = $calendar")
+        call_history[user] = calendar
+    }
+
+    fun updateCall_History(user: E) {
+        val calendar = Calendar.getInstance()
+        Plum.logger.debug("更新调用历史: user = $user, calendar = $calendar")
+        call_history[user] = calendar
+    }
+}
+
---
+++ src/main/kotlin/function/NudgeFunction.kt
@@ -0,0 +1,64 @@
+package com.sakurawald.plum.reloaded.function
+
+import com.sakurawald.PluginMain
+import com.sakurawald.api.HitoKoto_API
+import com.sakurawald.api.ThirdPartyRandomImage_API
+import com.sakurawald.debug.LoggerManager
+import com.sakurawald.framework.MessageManager
+import com.sakurawald.function.NudgeFunction
+import com.sakurawald.plum.reloaded.config.PlumConfig
+import com.sakurawald.utils.NetworkUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import net.mamoe.mirai.contact.Group
+import net.mamoe.mirai.event.events.NudgeEvent
+import net.mamoe.mirai.utils.ExternalResource.Companion.uploadAsImage
+
+object NudgeFunction : FunctionManager() {
+
+    fun handleEvent(event: NudgeEvent) {
+        if (!PlumConfig.functions.NudgeFunction.enable) return
+
+        val fromGroup = if (event.subject is Group) event.subject.id else -1
+        val fromQQ = event.from.id
+        val targetQQ = event.target.id
+
+        // Has Nudge Bot ?
+        if (targetQQ == PluginMain.getCurrentBot().id) {
+            /** 调用间隔合法检测.  */
+            if (!NudgeFunction.getInstance().canUse(fromGroup)) {
+                MessageManager.sendMessageBySituation(
+                    fromGroup,
+                    fromQQ,
+                    PlumConfig.functions.FunctionManager.callTooOftenMsg
+                )
+                LoggerManager.logDebug("NudgeFunction", "Call too often. Cancel!", true)
+                return
+            }
+            NudgeFunction.getInstance().updateUseTime(fromGroup)
+            var sendMsg = HitoKoto_API.getRandomSentence().formatedString
+            runBlocking(Dispatchers.IO) {
+                try {
+                    // Add RandomImage.
+                    val randomImageURL = ThirdPartyRandomImage_API.getInstance().randomImageURL
+                    val uploadImage = NetworkUtil.getInputStream(randomImageURL).uploadAsImage(
+                        PluginMain.getCurrentBot().groups.stream().findAny().get()
+                    )
+                    sendMsg += """
+                [mirai:image:${uploadImage.imageId}]
+                """.trimIndent()
+                } catch (e: Exception) {
+                    // Do nothing.
+                }
+                MessageManager.sendMessageBySituation(fromGroup, fromQQ, sendMsg)
+            }
+        }
+    }
+
+    override fun canUse(QQGroup: Long): Boolean {
+        val interval = PlumConfig.functions.NudgeFunction.perUseIntervalSecond
+        return functionUseHistoryManager.isCallSuccessIntervalLegal(
+            QQGroup, interval
+        )
+    }
+}
