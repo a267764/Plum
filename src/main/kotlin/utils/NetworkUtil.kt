72080779cdec3a6a831db340bc2c0116c8628e25
ヒューマンワーキング <coolxiaom95@gmail.com>
2022-10-30 15:53:33 +0800

    be more kotlin-friendly

--- src/main/kotlin/utils/NetworkUtil.kt
+++ src/main/kotlin/utils/NetworkUtil.kt
@@ -25,76 +25,46 @@ import javax.net.ssl.HttpsURLConnection
 object NetworkUtil {
     // [!] 这里设置与URL有关的变量，ENCODE表示目标网页的编码
     private const val ENCODE = "UTF-8"
-    fun deleteHTMLTags(htmlStr: String): String {
-        var htmlStr = htmlStr
-        val regEx_script = "<script[^>]*?>[\\s\\S]*?<\\/script>" // 定义script的正则表达式
-        val regEx_style = "<style[^>]*?>[\\s\\S]*?<\\/style>" // 定义style的正则表达式
-        val regEx_html = "<[^>]+>" // 定义HTML标签的正则表达式
-        // final String regEx_space = "\\s*|\t|\r|\n";// 定义空格回车换行符
-        val regEx_w = "<w[^>]*?>[\\s\\S]*?<\\/w[^>]*?>" // 定义所有w标签
-        val p_w = Pattern.compile(regEx_w, Pattern.CASE_INSENSITIVE)
-        val m_w = p_w.matcher(htmlStr)
-        htmlStr = m_w.replaceAll("") // 过滤script标签
-        val p_script = Pattern.compile(
-            regEx_script,
-            Pattern.CASE_INSENSITIVE
-        )
-        val m_script = p_script.matcher(htmlStr)
-        htmlStr = m_script.replaceAll("") // 过滤script标签
-        val p_style = Pattern
-            .compile(regEx_style, Pattern.CASE_INSENSITIVE)
-        val m_style = p_style.matcher(htmlStr)
-        htmlStr = m_style.replaceAll("") // 过滤style标签
-        val p_html = Pattern.compile(regEx_html, Pattern.CASE_INSENSITIVE)
-        val m_html = p_html.matcher(htmlStr)
-        htmlStr = m_html.replaceAll("") // 过滤html标签
+    const val userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
 
-        // 为了不破坏原始数据中的空白和换行，这里要注释掉
-        // Pattern p_space = Pattern.compile(regEx_space,
-        // Pattern.CASE_INSENSITIVE);
-        // Matcher m_space = p_space.matcher(htmlStr);
-        // htmlStr = m_space.replaceAll(""); // 过滤空格回车标签
-        // htmlStr = htmlStr.replaceAll(" ", ""); // 过滤
-        return htmlStr.trim { it <= ' ' } // 返回文本字符串
+    private val patternScript = Pattern.compile("<script[^>]*?>[\\s\\S]*?</script>", Pattern.CASE_INSENSITIVE)
+    private val patternStyle = Pattern.compile("<style[^>]*?>[\\s\\S]*?</style>", Pattern.CASE_INSENSITIVE)
+    private val patternHtml = Pattern.compile("<[^>]+>", Pattern.CASE_INSENSITIVE)
+    private val patternW = Pattern.compile("<w[^>]*?>[\\s\\S]*?</w[^>]*?>", Pattern.CASE_INSENSITIVE)
+    fun deleteHTMLTags(htmlStr: String): String {
+        var html = patternW.matcher(htmlStr).replaceAll("")
+        html =  patternScript.matcher(html).replaceAll("")
+        html = patternStyle.matcher(html).replaceAll("")
+        html = patternHtml.matcher(html).replaceAll("")
+
+        return html.trim { it <= ' ' } // 返回文本字符串
     }
 
     fun getStaticHTML(URL: String?): String {
-        val user_agent =
-            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
         val conn = Jsoup.connect(URL)
         // 修改http包中的header,伪装成浏览器进行抓取
-        conn.header("User-Agent", user_agent)
+        conn.header("User-Agent", userAgent)
         var doc: Document? = null
         try {
             doc = conn.get()
         } catch (e: IOException) {
             Plum.logger.error(e)
         }
-        return doc.toString()
+        return doc?.toString() ?: ""
     }
 
     fun betweenString(text: String, left: String?, right: String): String {
-        var result = ""
-        var zLen: Int
-        if (left == null || left.isEmpty()) {
-            zLen = 0
-        } else {
-            zLen = text.indexOf(left)
-            if (zLen > -1) {
-                zLen += left.length
-            } else {
-                zLen = 0
-            }
+        val zLen = if (left.isNullOrEmpty()) 0
+        else text.indexOf(left).let {
+            if(it > -1) it + left.length else 0
         }
-        var yLen = text.indexOf(right, zLen)
-        if (yLen < 0 || right.isEmpty()) {
-            yLen = text.length
+        val yLen = text.indexOf(right, zLen).let {
+            if (it < 0 || right.isEmpty()) text.length else it
         }
-        result = text.substring(zLen, yLen)
-        return result
+        return text.substring(zLen, yLen)
     }
 
-    fun getInputStream(file: File?): InputStream? {
+    fun getInputStream(file: File): InputStream? {
         try {
             return FileInputStream(file)
         } catch (e: FileNotFoundException) {
@@ -103,11 +73,9 @@ object NetworkUtil {
         return null
     }
 
-    fun getInputStream(URL: String?): InputStream? {
-        val URL_Object: URL
+    fun getInputStream(url: String?): InputStream? {
         try {
-            URL_Object = URL(URL)
-            val conn = URL_Object.openConnection() as HttpsURLConnection
+            val conn = URL(url).openConnection() as HttpsURLConnection
             conn.requestMethod = "GET"
             conn.connectTimeout = 10 * 1000
             return conn.inputStream
@@ -117,44 +85,34 @@ object NetworkUtil {
         return null
     }
 
-    fun decodeURL(str: String?): String {
-        var result = ""
-        if (null == str) {
-            return ""
-        }
+    fun decodeURL(str: String): String {
         try {
-            result = URLDecoder.decode(str, ENCODE)
+            return URLDecoder.decode(str, ENCODE)
         } catch (e: UnsupportedEncodingException) {
-            e.printStackTrace()
+            Plum.logger.error(e)
         }
-        return result
+        return str
     }
 
-    fun encodeURL(str: String?): String {
-        var result = ""
-        if (null == str) {
-            return ""
-        }
+    fun encodeURL(str: String): String {
         try {
-            result = URLEncoder.encode(str, ENCODE)
+            return URLEncoder.encode(str, ENCODE)
         } catch (e: UnsupportedEncodingException) {
-            e.printStackTrace()
+            Plum.logger.error(e)
         }
-        return result
+        return str
     }
 
     fun decodeHTML(htmlStr: String): String {
-        var htmlStr = htmlStr
-        htmlStr = htmlStr.replace("<br>", "\n").replace("&nbsp;", " ")
-            .replace("&gt;", "")
-        return htmlStr
+        return htmlStr.replace("<br>", "\n", true)
+            .replace("&nbsp;", " ", true)
+            .replace("&gt;", "", true)
     }
 
     fun getDynamicHTML(URL: String?): String? {
         try {
-            WebClient(BrowserVersion.CHROME).use { webClient ->
+            return WebClient(BrowserVersion.CHROME).use { webClient ->
                 /** WebClient Configs.  */
-
                 // 关闭WebClient错误警告.
                 LogFactory.getFactory().setAttribute(
                     "org.apache.commons.logging.Log",
@@ -165,17 +123,14 @@ object NetworkUtil {
 
                 //ajax
                 webClient.ajaxController = NicelyResynchronizingAjaxController()
-
                 //忽略css错误
                 webClient.cssErrorHandler = SilentCssErrorHandler()
-
                 webClient.options.run {
-                    //支持js
+                    //支持 js
                     isJavaScriptEnabled = true
-
-                    //忽略js错误
+                    //忽略 js 错误
                     isThrowExceptionOnScriptError = false
-                    //不执行CSS渲染
+                    //不执行 CSS 渲染
                     isCssEnabled = false
                     //超时时间
                     timeout = 10 * 1000
@@ -188,7 +143,7 @@ object NetworkUtil {
                 //开始请求网站
                 val page = webClient.getPage<HtmlPage>(URL)
                 val doc = Jsoup.parse(page.asXml(), URL)
-                return doc.toString()
+                return@use doc.toString()
             }
         } catch (e: Exception) {
             Plum.logger.error(e)
@@ -196,12 +151,10 @@ object NetworkUtil {
         return null
     }
 
-    fun downloadImageFile(image_URL: String?, path: String?) {
-        val url: URL
+    fun downloadImageFile(image_URL: String, path: String) {
         try {
-            url = URL(image_URL)
             val dataInputStream = DataInputStream(
-                url.openStream()
+                URL(image_URL).openStream()
             )
             val fileOutputStream = FileOutputStream(path)
             val output = ByteArrayOutputStream()
@@ -214,35 +167,29 @@ object NetworkUtil {
             dataInputStream.close()
             fileOutputStream.close()
         } catch (e: IOException) {
-            e.printStackTrace()
+            Plum.logger.error(e)
         }
     }
 
     @Throws(FileTooBigException::class, CanNotDownloadFileException::class)
-    fun downloadVoiceFile(urlPath: String, downloadDir: String?): String? {
+    fun downloadVoiceFile(urlPath: String, downloadDir: String) {
         return downloadFile(
             urlPath,
             downloadDir,
-            PlumConfig.functions.SingSongFunction.maxVoiceFileSize
+            PlumConfig.functions.singSongFunction.maxVoiceFileSize
         )
     }
 
     @Throws(CanNotDownloadFileException::class, FileTooBigException::class)
-    fun downloadFile(urlPath: String, downloadDir: String?, acceptFileMaxLength: Int): String? {
-        var file: File? = null
-        var path: String? = null
+    fun downloadFile(urlPath: String, downloadDir: String, acceptFileMaxLength: Int) {
         try {
-            val url = URL(urlPath)
-            val urlConnection = url.openConnection()
+            val urlConnection = URL(urlPath).openConnection()
             val httpURLConnection = urlConnection as HttpURLConnection
             httpURLConnection.requestMethod = "GET"
             httpURLConnection.setRequestProperty("Charset", "UTF-8")
             httpURLConnection.connect()
             val fileLength = httpURLConnection.contentLength
-            Plum.logger.debug(
-                "NetworkSystem >> Download: URL_path = " + urlPath
-                        + ", fileLength = " + fileLength
-            )
+            Plum.logger.debug("NetworkSystem >> Download: URL_path = $urlPath, fileLength = $fileLength")
             /**
              * [!] 判断是否为付费歌曲。 若一首歌曲是付费歌曲，则网易云的音乐下载链接会404
              */
@@ -252,28 +199,15 @@ object NetworkUtil {
             if (fileLength > acceptFileMaxLength) {
                 throw FileTooBigException()
             }
-            val bin = BufferedInputStream(
-                httpURLConnection.inputStream
-            )
-            path = downloadDir
-            println(path)
-            file = File(path)
+            val input = httpURLConnection.inputStream
+            val file = File(downloadDir)
             if (!file.parentFile.exists()) {
                 file.parentFile.mkdirs()
             }
-            val out: OutputStream = FileOutputStream(file)
-            var size = 0
-            var len = 0
-            val buf = ByteArray(1024)
-            while (bin.read(buf).also { size = it } != -1) {
-                len += size
-                out.write(buf, 0, size)
-            }
-            bin.close()
-            out.close()
+            val output: OutputStream = FileOutputStream(file)
+            output.write(input.readBytes().also { input.close() }).also { output.close() }
         } catch (e: Exception) {
             Plum.logger.error(e)
         }
-        return null
     }
 }
