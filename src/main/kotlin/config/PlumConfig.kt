4b6267696a6fd33c9cd1a6284c47b17257eb4c86
ヒューマンワーキング <coolxiaom95@gmail.com>
2022-10-30 16:55:56 +0800

    fix config

--- src/main/kotlin/config/PlumConfig.kt
+++ src/main/kotlin/config/PlumConfig.kt
@@ -1,5 +1,6 @@
 package com.sakurawald.plum.reloaded.config
 
+import kotlinx.serialization.Serializable
 import net.mamoe.mirai.console.data.ReadOnlyPluginConfig
 import net.mamoe.mirai.console.data.value
 
@@ -8,131 +9,225 @@ import net.mamoe.mirai.console.data.value
  * src/main/java/com/sakurawald/files/ApplicationConfig_Data.java
  */
 object PlumConfig : ReadOnlyPluginConfig("config") {
-    class PlumConfigDebug {
-        val enable by value(false)
-    }
-
-    class PlumConfigAdmin {
-        class AdminInvitationManager {
-            class IMQQFriendInvitation {
-                val autoAcceptAddQQFriend by value(false)
-            }
-
-            class IMQQGroupInvitation {
-                val autoAcceptAddQQGroup by value(false)
-            }
-
-            val friendInvitation by value(IMQQFriendInvitation())
-            val groupInvitation by value(IMQQGroupInvitation())
-        }
-
-        class AdminRobotControl {
-            var forceCancelGroupMessage by value(false)
-            var forceCancelFriendMessage by value(true)
-        }
-
-        val invitationManager by value(AdminInvitationManager())
-        val robotControl by value(AdminRobotControl())
-        val botAdministrators by value(listOf(3172906506L))
-    }
-
-    class PlumConfigFunctions {
-        class FunctionsFunctionManager {
-            var callTooOftenMsg by value("调用该功能过于频繁，请稍后再试试吧~")
-            var functionDisableMsg by value("很抱歉，该功能已被禁用。")
-        }
-
-        class FunctionsDailyCountdown {
-            var enable by value(true)
-            var countdownCommands by value(
-                listOf(
+    val debug by value(
+        PlumConfigDebug(
+            enable = false
+        )
+    )
+    val admin by value(
+        PlumConfigAdmin(
+            invitationManager = AdminInvitationManager(
+                friendInvitation = IMQQFriendInvitation(
+                    autoAcceptAddQQFriend = false
+                ),
+                groupInvitation = IMQQGroupInvitation(
+                    autoAcceptAddQQGroup = false
+                )
+            ),
+            robotControl = AdminRobotControl(
+                forceCancelGroupMessage = false,
+                forceCancelFriendMessage = true
+            ),
+            botAdministrators = listOf(
+                2431208142L
+            )
+        )
+    )
+    val functions by value(
+        PlumConfigFunctions(
+            functionManager = FunctionsFunctionManager(
+                callTooOftenMsg = "调用该功能过于频繁，请稍后再试试吧~",
+                functionDisableMsg = "很抱歉，该功能已被禁用。"
+            ),
+            dailyCountdown = FunctionsDailyCountdown(
+                enable = true,
+                countdownCommands = listOf(
                     "◆距离2021年高考还有\$diff_days天！|1623027804000|高考加油！&高考加油！&2021年高考已结束~",
                     "◆距离2020年考研还有\$diff_days天！|1608512604000|考研加油&考研加油&考研加油&2020年考研已结束~",
                     "◆距离2020年四六级考试还有\$diff_days天！|1600477404000|四六级考试加油！&四六级考试已结束~"
                 )
-            )
-        }
-
-        class FunctionsDailyPoetry {
-            class DPJinRiShiCi {
-                var token by value("paOa0DqOdpLn4FVVHNtEDgU5Imk89kXZ")
-            }
-
-            val jinRiShiCi by value(DPJinRiShiCi())
-            var explanationEnable by value(true)
-            var maxRetryLimit by value(3)
-        }
-
-        class FunctionsAtFunction {
-            val enable by value(false)
-
-            class AFRandomImage {
-                var randomImageURLs by value(
-                    listOf(
+            ),
+            dailyPoetry = FunctionsDailyPoetry(
+                jinRiShiCi = DPJinRiShiCi(
+                    token = "paOa0DqOdpLn4FVVHNtEDgU5Imk89kXZ"
+                ),
+                explanationEnable = true,
+                maxRetryLimit = 3
+            ),
+            atFunction = FunctionsAtFunction(
+                enable = false,
+                randomImage = AFRandomImage(
+                    randomImageURLs = listOf(
                         "http://www.dmoe.cc/random.php",
                         "http://api.mtyqx.cn/tapi/random.php",
                         "http://api.mtyqx.cn/api/random.php"
                     )
                 )
-            }
-
-            val randomImage by value(AFRandomImage())
-        }
-
-        class FunctionsNudgeFunction {
-            class NFHitoKoto {
-                var getURLParams by value("")
-            }
-
-            var enable by value(true)
-            var perUseIntervalSecond by value(3)
-            val hitoKoto by value(NFHitoKoto())
-        }
-
-        class FunctionsSingSongFunction {
-            var enable by value(true)
-            var forceSendCard by value(false)
-            var perUseIntervalSecond by value(5)
-            var maxVoiceFileSize by value(15000000)
-            var msgMusicNeedPaid by value("很抱歉，这可能是一首付费歌曲。")
-            var msgDownloadMusicFileTooBig by value("这首歌太长啦！")
-            var msgNotFoundMusic by value("哎呀，咱还不会《[OBJECT1]》这首歌！")
-        }
-
-        val functionManager by value(FunctionsFunctionManager())
-        val dailyCountdown by value(FunctionsDailyCountdown())
-        val dailyPoetry by value(FunctionsDailyPoetry())
-        val atFunction by value(FunctionsAtFunction())
-        val nudgeFunction by value(FunctionsNudgeFunction())
-        val singSongFunction by value(FunctionsSingSongFunction())
-    }
-
-    class PlumConfigSystem {
-        class SystemSendSystem {
-            class SSendDelay {
-                class SDSendToFriend {
-                    var enable by value(false)
-                    var delayTimeMS by value(0L)
-                }
-
-                class SDSendToGroup {
-                    var enable by value(true)
-                    var delayTimeMS by value(1000L)
-                }
-
-                val sendToFriend by value(SDSendToFriend())
-                val sendToGroup by value(SDSendToGroup())
-            }
-
-            val sendDelay by value(SSendDelay())
-            val sendMsgMaxLength by value(4500)
-        }
-
-        val sendSystem by value(SystemSendSystem())
-    }
-
-    val debug by value(PlumConfigDebug())
-    val admin by value(PlumConfigAdmin())
-    val functions by value(PlumConfigFunctions())
-    val system by value(PlumConfigSystem())
+            ),
+            nudgeFunction = FunctionsNudgeFunction(
+                enable = true,
+                perUseIntervalSecond = 3,
+                hitoKoto = NFHitoKoto(
+                    getURLParams = ""
+                )
+            ),
+            singSongFunction = FunctionsSingSongFunction(
+                enable = true,
+                forceSendCard = false,
+                perUseIntervalSecond = 5,
+                maxVoiceFileSize = 15000000,
+                msgMusicNeedPaid = "很抱歉，这可能是一首付费歌曲。",
+                msgDownloadMusicFileTooBig = "这首歌太长啦！",
+                msgNotFoundMusic = "哎呀，咱还不会《[OBJECT1]》这首歌！"
+            )
+        )
+    )
+    val system by value(
+        PlumConfigSystem(
+            sendSystem = SystemSendSystem(
+                sendDelay = SSendDelay(
+                    sendToFriend = SDSendToFriend(
+                        enable = false,
+                        delayTimeMS = 0L
+                    ),
+                    sendToGroup = SDSendToGroup(
+                        enable = true,
+                        delayTimeMS = 1000L
+                    )
+                ),
+                sendMsgMaxLength = 4500
+            )
+        )
+    )
 }
+
+@Serializable
+data class PlumConfigDebug(
+    val enable: Boolean
+)
+
+@Serializable
+data class PlumConfigAdmin(
+    val invitationManager: AdminInvitationManager,
+    val robotControl: AdminRobotControl,
+    val botAdministrators: List<Long>
+)
+
+@Serializable
+data class AdminInvitationManager(
+    val friendInvitation: IMQQFriendInvitation,
+    val groupInvitation: IMQQGroupInvitation
+)
+
+@Serializable
+data class IMQQFriendInvitation(
+    val autoAcceptAddQQFriend: Boolean
+)
+
+@Serializable
+data class IMQQGroupInvitation(
+    val autoAcceptAddQQGroup: Boolean
+)
+
+@Serializable
+data class AdminRobotControl(
+    var forceCancelGroupMessage: Boolean,
+    var forceCancelFriendMessage: Boolean
+)
+
+@Serializable
+data class PlumConfigFunctions(
+    val functionManager: FunctionsFunctionManager,
+    val dailyCountdown: FunctionsDailyCountdown,
+    val dailyPoetry: FunctionsDailyPoetry,
+    val atFunction: FunctionsAtFunction,
+    val nudgeFunction: FunctionsNudgeFunction,
+    val singSongFunction: FunctionsSingSongFunction,
+)
+
+@Serializable
+data class FunctionsFunctionManager(
+    var callTooOftenMsg: String,
+    var functionDisableMsg: String
+)
+
+@Serializable
+data class FunctionsDailyCountdown(
+    var enable: Boolean,
+    var countdownCommands: List<String>
+)
+
+@Serializable
+data class FunctionsDailyPoetry(
+    val jinRiShiCi: DPJinRiShiCi,
+    var explanationEnable: Boolean,
+    var maxRetryLimit: Int
+)
+
+@Serializable
+data class DPJinRiShiCi(
+    var token: String
+)
+
+@Serializable
+class FunctionsAtFunction(
+    val enable: Boolean,
+    val randomImage: AFRandomImage
+)
+
+@Serializable
+data class AFRandomImage(
+    var randomImageURLs: List<String>
+)
+
+@Serializable
+data class FunctionsNudgeFunction(
+    var enable: Boolean,
+    var perUseIntervalSecond: Int,
+    val hitoKoto: NFHitoKoto
+)
+
+@Serializable
+data class NFHitoKoto(
+    var getURLParams: String
+)
+
+@Serializable
+data class FunctionsSingSongFunction(
+    var enable: Boolean,
+    var forceSendCard: Boolean,
+    var perUseIntervalSecond: Int,
+    var maxVoiceFileSize: Int,
+    var msgMusicNeedPaid: String,
+    var msgDownloadMusicFileTooBig: String,
+    var msgNotFoundMusic: String
+)
+
+@Serializable
+data class PlumConfigSystem(
+    val sendSystem: SystemSendSystem
+)
+
+@Serializable
+data class SystemSendSystem(
+    val sendDelay: SSendDelay,
+    val sendMsgMaxLength: Int
+)
+
+@Serializable
+data class SSendDelay(
+    val sendToFriend: SDSendToFriend,
+    val sendToGroup: SDSendToGroup
+)
+
+@Serializable
+data class SDSendToFriend(
+    var enable: Boolean,
+    var delayTimeMS: Long
+)
+
+@Serializable
+data class SDSendToGroup(
+    var enable: Boolean,
+    var delayTimeMS: Long
+)
